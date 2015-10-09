9185c42bb9bfc87fe92b75f7de96a1217b4240cc
benjamin1995 <batibenji.07@gmail.com>
2015-10-09 01:45:41 -0300

    rk2 con errores explicitos

--- rk2.py
+++ rk2.py
@@ -9,14 +9,15 @@ import matplotlib.pyplot as plt
 
 
 A = np.pi / 30
-w = np.sqrt(10)
+w1 = np.sqrt(10)
 
 plt.figure(1)
 plt.clf()
 
-t = np.linspace(0, 5 * 2 * np.pi / w, 400)
+t = np.linspace(0, 5 * 2 * np.pi / w1, 400)
 
-plt.plot(t, A * np.sin(w * t))
+plt.plot(t, A * np.cos(w1 * t),'r',label="teorico")   # ERROR! ya que phi inicial es A, no podia ser seno.
+
 
 
 def f(phi, w):
@@ -29,12 +30,12 @@ def get_k1(phi_n, w_n, h, f):
 def get_k2(phi_n, w_n, h, f):
     k1 = get_k1(phi_n, w_n, h, f)
     f_eval = f(phi_n + k1[0]/2, w_n + k1[1]/2)
-    return h * f_eval[0], h * f_eval[1]
+    return k1,(h * f_eval[0], h * f_eval[1]) # ERROR! faltaba retornar k1 para ocuparlo en rk2_step, en la parte de phi_n1 y w_n1 que requerian de k1.
 
 def rk2_step(phi_n, w_n, h, f):
-    k2 = get_k2(phi_n, w_n, h, f)
-    phi_n1 = phi_n + k2[0] * h
-    w_n1 = w_n + k2[1] * h
+    k1,k2 = get_k2(phi_n, w_n, h, f)
+    phi_n1 = phi_n + (k2[0]+k1[0])/2   # ERROR! en esta parte se habia definido mal phi_n1 y w_n1
+    w_n1 = w_n + (k2[1]+k1[1])/2
     return phi_n1, w_n1
 
 N_steps = 40000
@@ -42,7 +43,7 @@ h = 10. / N_steps
 phi = np.zeros(N_steps)
 w = np.zeros(N_steps)
 
-phi[0] = A
+phi[0] = A   # ERROR! dada esta definicion es que se tuvo que cambiar a coseno el grafico inicial
 w[0] = 0
 for i in range(1, N_steps):
     phi[i], w[i] = rk2_step(phi[i-1], w[i-1], h, f)
@@ -51,12 +52,12 @@ for i in range(1, N_steps):
 
 t_rk = [h * i for i in range(N_steps)]
 
-plt.plot(t_rk, phi, 'g')
+plt.plot(t_rk, phi, 'g',label='metodo rk2')
 
 
-
-
-plt.xlabel('tiempo')
-plt.ylabel('$\phi(t)$', fontsize=18)
+plt.title('phi vs tiempo ',fontsize=14)
+plt.xlabel('$\ tiempo[seg] $')
+plt.ylabel('$\phi(t) [rad] $', fontsize=13)
+plt.legend()
 plt.show()
 plt.draw()
